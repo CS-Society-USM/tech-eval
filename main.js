84ed36d718dc81ab58f2ceaf219e5a5b66a37100
Lim Wen Hao <wenhaojshs@gmail.com>
2023-10-27 19:19:12 +0800

    update index.html and main.js

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Hello Applicants!</h1>
+
+    <h3>Hi. I am Lim Wen Hao from Year 1 Sem 1, student of School of Computer Science.</h3>
+    <p>To know about me, here is my portfolio. 👇</p>
+    <p><a href="https://wenhao1223.github.io/WenHao1223" target="_blank">Self-coded Portfolio</a></p>
+
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
